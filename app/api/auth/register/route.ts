cdeb771f3372f660a09972e1e2092650702bc97c
Dimitris Alexiou <dalexiou@netcompany.com>
2024-07-17 00:20:04 +0300

    register functionality, mongodb, prisma setup

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,31 @@
+import bcrypt from 'bcrypt';
+
+import prisma from '@/app/libs/prismadb';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+	const body = await request.json();
+	const { email, name, password } = body;
+
+	const existingUser = await prisma.user.findUnique({
+		where: { email },
+	});
+
+	if (existingUser)
+		return NextResponse.json(
+			{ error: 'User already exists.' },
+			{ status: 400 }
+		);
+
+	const hashedPassword = await bcrypt.hash(password, 12);
+
+	const user = await prisma.user.create({
+		data: {
+			email,
+			name,
+			hashedPassword,
+		},
+	});
+
+	return NextResponse.json(user);
+}
