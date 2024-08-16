9223b27c13dcae43cee17eda17def84dc09bf4c4
Dimitris Alexiou <dalexiou199524@gmail.com>
2024-08-16 21:34:47 +0300

    .env config changes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -53,7 +53,7 @@ export const authOptions: AuthOptions = {
 	session: {
 		strategy: 'jwt',
 	},
-	secret: process.env.NEXTAUTH_SECRET,
+	secret: process.env.AUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
